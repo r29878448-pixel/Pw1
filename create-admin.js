803cfa981b0e8e1d80ff2ac9eb1e7cdc45e3295d
r29878448-pixel <r29878448@gmail.com>
2026-05-05 16:01:32 +0530

    Update create-admin.js

--- create-admin.js
+++ create-admin.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   messagingSenderId: "255162339734",
   appId: "1:255162339734:web:f49f464c93c63bc280cdb7",
   measurementId: "G-5YX2GJKD6B"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
